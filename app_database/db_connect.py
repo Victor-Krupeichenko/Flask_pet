172c2aa0103d40bb12a0861c890f4b4a9380c53d
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-07-17 19:09:22 +0300

    Подключение к базе данных

---
+++ app_database/db_connect.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from app_database.url_database import _URL
+
+engine = create_engine(_URL)
+session_maker = sessionmaker(engine, expire_on_commit=False)
