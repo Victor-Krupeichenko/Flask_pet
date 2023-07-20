db4e2a5e167d2a43d3493217397d77e2ecb091e8
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-07-20 15:08:56 +0300

    Форма для создания поста

--- post/forms.py
+++ post/forms.py
@@ -1,17 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length
-from app_database.models import Category
-from sqlalchemy import select
-from app_database.db_connect import session_maker
-
-with session_maker() as db_session:
-    try:
-        category_set = list()
-        for category in db_session.scalars(select(Category)).all():
-            category_set.append((category.id, category.title))
-    except Exception as ex:
-        raise SystemError(f"Error connect database {ex}")
 
 
 class PostForm(FlaskForm):
@@ -20,7 +9,7 @@ class PostForm(FlaskForm):
         DataRequired(), Length(min=3, max=40, message="Title must be between 3 and 40 characters")
     ])
     content = TextAreaField("Content:", validators=[DataRequired()], render_kw={"rows": 10})
-    category_id = SelectField("Category:", validators=[DataRequired()], choices=category_set)
+    category_id = SelectField("Category:", validators=[DataRequired()])
     submit = SubmitField("Submit:")
 
 
